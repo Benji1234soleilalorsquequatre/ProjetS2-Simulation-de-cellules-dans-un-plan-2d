60b646f5a494ccf05deb01855a36f362a2ad8e1b
PsyCh0Ls <bababagayoko75@gmail.com>
2026-05-26 13:57:02 +0200

    Add files via upload

--- propagation-feu/src/model/Grid.java
+++ propagation-feu/src/model/Grid.java
@@ -1,3 +1,7 @@
+/**
+ * The Grid class represents a grid structure with cells initialized to a safe state and specific
+ * parameters.
+ */
 package model;
 public class Grid{
     private Cell[][] forest;
@@ -10,6 +14,15 @@ public class Grid{
         for(int i = 0; i < height; i++){
             for(int j = 0; j < width; j++){
                 this.forest[i][j] = new Cell(State.SAFE,40, 0);
+            }
+        }
+    }
+    public void displayGrid(){
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++){
+                State currentState=this.forest[i][j].getCurrentState();
+                
+
             }
         }
     }
